fc0f92bd319896d371e90efcde3b8687ccd8237b
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-09-27 04:17:36 +0100

    Add files via upload

--- cd.sh
+++ cd.sh
@@ -1,3 +1,5 @@
+bcc -c -Md libdos.c -o libdos.a
+bcc -x -i -L -Md hello.c -o hello.com
 mkdir -p CD_root/isolinux
 mkdir -p CD_root/images
 mkdir -p CD_root/kernel
@@ -10,16 +12,5 @@ cp *.com CD_root/isolinux/
 cp *.elf CD_root/isolinux/
 cp *.jpg CD_root/isolinux/
 cp *.menu CD_root/isolinux/
-cp *.txt CD_root/isolinux/
-nasm -felf32 -o boot.o boot.S
-nasm -o hello.com hello.asm
-gcc -c kernel.c -o kernel.o -nostdlib
-gcc link.ld boot.o kernel.o -o kernel.elf -nostdlib
-nasm model.asm -o model.o
-objdump -D kernel.elf > out.txt
-cat out.txt | grep kernel_main
-dd if=kernel.elf of=hello.c32 
-dd if=model.o of=hello.c32 count=1 conv=notrunc
-cp *.c32 CD_root/isolinux/
 genisoimage -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
 
